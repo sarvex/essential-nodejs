1226f215dc82d2ad32529c281e65251d2e4446c8
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2024-06-15 19:17:35 +0530

    sync

--- 02_01/begin/global.js
+++ 02_01/begin/global.js
@@ -1,5 +1,2 @@
 let hello = 'Hello World from Node.js';
 console.log(hello);
-
-console.log(__dirname);
-console.log(__filename);
--- 02_03/begin/global.js
+++ 02_03/begin/global.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 console.log(__dirname);
-console.log(
-  `The file name is ${path.basename(__filename)}`
-);
+console.log(`The file name is ${path.basename(__filename)}`);
 
 for (let key in global) {
   console.log(key);
 }
+
+console.log(process.argv);
